c95d30949a06a0ede9613be965b625d3ddf1fd77
wentianfan <wentianfan@gmail.com>
2023-01-26 21:29:48 +0000

    Add teacher's code

--- project/web/modules/custom/hello_world/hello_world.module.php
+++ project/web/modules/custom/hello_world/hello_world.module.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Implement hook_form_alter
+ * Implements hook_form_alter().
  */
 
 function hello_world_form_alter (&$form, \Drupal\Core\Form\FormStateInterface &$form_state, $form_id){
@@ -13,5 +13,45 @@ function hello_world_form_alter (&$form, \Drupal\Core\Form\FormStateInterface &$
     if ($form_id === 'node_artical_form') {
         ksm($form);
         $form['options']['#open'] = TRUE;
+    }
+}
+
+function hello_world_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+    if ($form_id === 'node_article_form') {
+    $forms = [
+      'node_page_form',
+      'node_article_form',
+    ];
+    if (in_array($form_id, $forms)) {
+      $form['options']['#open'] = TRUE;
+    }
+  }   
+}
+/**
+ * Implements hook_form_FORM_ID_alter().
+ *
+ * Change title of Promotion Options element in node article form.
+ */
+function hello_world_form_node_article_form_alter (&$form, \Drupal\Core\Form\FormStateInterface &$form_state, $form_id){
+    $form['options']['title'] = "Promotion options for the articles";
+    //they have the same function
+    //$user = \Drupal::currentUser();
+    //$user = \Drupal::service('current_user');
+    $user = \Drupal::currentUser();
+    $form['field_tags']['#access'] = $user->hasPermission('administer content');
+    //same function as above two lines
+    //$form['field_tags']['#access'] = \Drupal::currentUer()->hasPermission('administer content');
+    
+}
+
+function hello_world_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+    if ($form_id === 'node_article_form') {
+        $forms = [
+            'node_page_form',
+            'node_article_form',
+        ];
+        if (in_array($form_id, $forms)) {
+            $form['options']['#open'] = TRUE;
+        }
     }
 }
--- project/web/modules/custom/hello_world/src/Controller/HelloController.php
+++ project/web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -36,33 +36,52 @@ class HelloController extends ControllerBase {
 
   public function helloNameNode($name, $nid){
     //ksm($name);
-    //ksm(gettype($nid));    
-    $node= Node::load($nid);
-      if (!$node){
-        $output = $this->t("The @input is not a valid page id.", ['@input' =>$nid]);
-        return [
-          '#type' => 'markup',
-          '#markup' => $output,
-        ];
-      }    
-    
+    //ksm(gettype($nid)); 
     //ksm($node->getTitle());
-    //ksm($node->toLink());
-    $link = $node->toLink();
-  
+    //ksm($node->toLink());  
+    //following lines are the same function as $link = $node->toLink();
     //$output = $this->t("Hi @person! The title of this node is @title", ['@person' => $name, '@title' => $node->getTitle()]);
     //$url = Url::fromRoute('entity.node.canoical', ['node' =>$nid]);
 	  //ksm($url);
 	  //$link = Link::fromTestAndUrl($node->getTitle(), $url);
-	  //ksm($link);
-       
-    $output = $this->t("Hi @person! The title of this node is @title", ['@person' => $name, '@title' => $link->toString()]);
-        
+	  //ksm($link); 
+    $node= Node::load($nid);
+    if (!$node){
+      $output = $this->t("The @input is not a valid page id.", ['@input' =>$nid]);
+      return [
+        '#type' => 'markup',
+        '#markup' => $output,
+      ];
+    }      
+      $link = $node->toLink();
+      $output = $this->t("Hi @person! The title of this node is @title", ['@person' => $name, '@title' => $link->toString()]);
+      return [
+        '#type' => 'markup',
+        '#markup' => $output,
+      ];
+  }
+
+  //teacher's code
+  $output = [];
+    if (is_numeric($nid)) {
+      $node = Node::load($nid);
+      if ($node) {
+        $title = $node->toLink()->toString();
+        $output = $this->t('Hello @name! The title of the node is @title', ['@name' => $name, '@title' => $title]);
+      }
+      else {
+        $output = $this->t('Hey :name! That nid does not exists!', [':name' => $name]);
+      }
+    }
+    else {
+      $output = $this->t('Hey :name! That nid should be a number!', [':name' => $name]);
+    }
+
     return [
       '#type' => 'markup',
       '#markup' => $output,
     ];
   }
-                                   
+
 }    
 
