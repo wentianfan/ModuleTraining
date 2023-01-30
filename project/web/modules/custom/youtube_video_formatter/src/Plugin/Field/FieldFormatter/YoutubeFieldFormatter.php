002ab8e93ddf1bd0e21cd9f9dfb303c4a770445b
wentianfan <wentianfan@gmail.com>
2023-01-30 16:33:09 +0000

    Add youtube video formatter

--- project/web/modules/custom/youtube_video_formatter/src/Plugin/Field/FieldFormatter/YoutubeFieldFormatter.php
+++ project/web/modules/custom/youtube_video_formatter/src/Plugin/Field/FieldFormatter/YoutubeFieldFormatter.php
@@ -28,12 +28,48 @@ class YoutubeFieldFormatter extends FormatterBase {
     foreach ($items as $delta => $item) {
       $elements[$delta] = [
         '#theme' => 'youtube_video',
-        '#youtube_id' => $item->value, 
+        '#youtube_id' => $item->value,
+        '#width' => $this->getSettings('width'), 
       ];
     }
 
     return $elements;
   }
+
+  /**
+   * {@inheritdoc} 
+   */
+
+  public static function defaultSettings() {
+    return[
+      'width' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm (array $form, FormStateInterface $form_state ){
+    $element['width'] = [
+      'type' => 'number',
+      'title' => 'video width',
+      'default_value' => '560',
+      '#required' => TRUE,
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary(){
+    $summary = [];
+
+    $summary[] = 'The width is'. $this->getSettings('width');
+
+    return $summary;
+  } 
 
 }
 
