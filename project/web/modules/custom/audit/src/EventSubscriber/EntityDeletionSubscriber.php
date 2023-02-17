e99d0838b6bbeeea06ca54bc2c751785a6a4cf44
wentianfan <wentianfan@gmail.com>
2023-02-17 18:13:38 +0000

    Add entity details.

--- project/web/modules/custom/audit/src/EventSubscriber/EntityDeletionSubscriber.php
+++ project/web/modules/custom/audit/src/EventSubscriber/EntityDeletionSubscriber.php
@@ -21,6 +21,32 @@ class EntityDeletionSubscriber implements EventSubscriberInterface {
    * @param Drupal\core_event_dispatcher\Event\Entity\EntityDeleteEvent $event
    */
   public function logDeletion(EntityDeleteEvent $event) {
-    \Drupal::logger('audit')->notice('It works.');    
+    $entity = $event->getEntity();
+    $entity_type = $entity->getEntityTypeId();
+
+    if ($entity instanceof ConfigEntityInterface){
+      return;
+    }
+    if ($entity_type === 'path_alias'){
+      return;
+    }
+    $data = [
+      'lable' => $entity->label(),
+      'created' => $entity->created->value,
+      'deleted' => time(),
+      'deleted_by' => \Drupal::currentUser()->id(),
+      'entity_type' => $entity_type,
+      'bundle' => $entity->bundle,
+    ];
+    if (isset($entity->uid) && $entity_type != 'user' ){
+      $data['uid'] = $entity->uid;
+    }
+    if (isset($entity->changed)){
+      $data['changed'] = $entity->changed->value;
+    }
+
+    $record = \Drupal::entityTypeManager()->getStoryage('deletion_record')->create($data);
+    $record->save();
+        //\Drupal::logger('audit')->notice('It works.');    
   }
 }
